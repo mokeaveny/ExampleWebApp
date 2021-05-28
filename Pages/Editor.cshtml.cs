0c1c5f20d1da99c00ccdd1873798bb7c8e4e1500
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-05-28 16:19:58 +0100

    Further razor page work. Added a layout page for multiple razor pages. As well as the creation of a razor page without a page model

--- Pages/Editor.cshtml.cs
+++ Pages/Editor.cshtml.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ExampleWebApp.Models;
 
 namespace ExampleWebApp.Pages
 {
     public class EditorModel : PageModel
     {
-        public void OnGet()
+        private DataContext context;
+
+        public Product Product { get; set; }
+
+        public EditorModel(DataContext ctx)
         {
+            context = ctx;
+        }
+
+        public async Task OnGetAsync(long id)
+        {
+            Product = await context.Products.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPostAsync(long id, decimal price)
+        {
+            Product p = await context.Products.FindAsync(id);
+            p.Price = price;
+            await context.SaveChangesAsync();
+            return RedirectToPage();
         }
     }
 }
