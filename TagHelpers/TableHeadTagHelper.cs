a1f04ecf4a715f2ba734302ff039516b8c47b7a3
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-05-31 11:27:56 +0100

    Created a custom HTML element called tablehead

---
+++ TagHelpers/TableHeadTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ExampleWebApp.TagHelpers
+{
+    [HtmlTargetElement("tablehead")]
+    public class TableHeadTagHelper : TagHelper
+    {
+        public string BgColor { get; set; } = "light";
+
+        public override async Task ProcessAsync(TagHelperContext context,
+            TagHelperOutput output)
+        {
+            output.TagName = "thead";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("class",
+                $"bg-{BgColor} text-white text-center");
+
+            string content = (await output.GetChildContentAsync()).GetContent();
+            output.Content
+                .SetHtmlContent($"<tr><th colspan=\"2\">{content}</th></tr>");
+        }
+    }
+}
