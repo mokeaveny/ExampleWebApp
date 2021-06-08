be79dcdafddd088592f2a5eb8a04f89d47ad2234
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-06-08 18:12:02 +0100

    Refactored the code so that model valdiation is performed using metadata.Therefore the validation constraints for particular fields aren't explicitly coded

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,21 +5,28 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ExampleWebApp.Validation;
 
 namespace ExampleWebApp.Models
 {
+    [PhraseAndPrice(Phrase = "Small", Price = "100")]
     public class Product
     {
         public long ProductId { get; set; }
 
+        [Required]
+        [Display(Name = "Name")]
         public string Name { get; set; }
         [Column(TypeName = "decimal(8, 2)")]
-        //[BindNever]
+        [Required(ErrorMessage = "Please enter a price")]
+        [Range(1, 999999, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
 
+        [PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Category))]
         public long CategoryId { get; set; }
         public Category Category { get; set; }
 
+        [PrimaryKey(ContextType = typeof(DataContext), DataType = typeof(Supplier))]
         public long SupplierId { get; set; }
         public Supplier Supplier { get; set; }
     }
