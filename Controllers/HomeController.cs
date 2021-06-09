9a6e4f6f81aac9f7e5c53e5eb9e62ff0beee8b24
Michael Keaveny <michaelkeaveny0@gmail.com>
2021-06-09 21:29:36 +0100

    Further filters. This includes action filters and result filters

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using ExampleWebApp.Filters;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace ExampleWebApp.Controllers
 {
     [HttpsOnly]
+    [ResultDiagnostics]
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -19,6 +21,20 @@ namespace ExampleWebApp.Controllers
         public IActionResult Secure()
         {
             return View("Message", "This is the Secure action on the Home controller");
+        }
+
+        //[ChangeArg]
+        public IActionResult Messages(string message1, string message2 = "None")
+        {
+            return View("Message", $"{message1}, {message2}");
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionArguments.ContainsKey("message1"))
+            {
+                context.ActionArguments["message1"] = "New message";
+            }
         }
     }
 }
